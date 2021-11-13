b0cdc61eaca6a31d4e4f916c41bc33fe085eff36
FrankXav <88958106+FrankXav@users.noreply.github.com>
2021-11-12 18:21:27 -0600

    Agrego script python

---
+++ scriptgit.py
@@ -0,0 +1,18 @@
+import subprocess as subp
+
+#subp.run(["ls"])
+
+resp1 = "si"#input("Agrego algun archivo nuevo?: (si o no)  ")
+
+comentario = "Agrego script python"#input("Comentario del nuevo commit: ")
+
+nomrama = "frankrama"#input("Nombre de la rama actual: ")
+
+if (resp1 == "si"):
+	subp.run(["git", "add", "."])
+
+subp.run(["git", "commit", f'-am {comentario}'])  #f'commit -am "{comentario}"'
+
+#subp.run(["git",f'push origin {nomrama}'])
+
+print("Cambios Actualizados")
